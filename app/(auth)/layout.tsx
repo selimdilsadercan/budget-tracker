dca18b41c8aeaedf1f2bf4bb670e38fab7b0c479
selimdilsad <selimdilsadercan@gmail.com>
2024-05-01 12:34:55 +0300

    feat: created logo component and added to auth layout

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,10 @@
+import Logo from "@/components/global/Logo";
+
 function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      {children}
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <Logo type="normal" />
+      <div className="mt-12">{children}</div>
     </div>
   );
 }
---
+++ components/global/Logo.tsx
@@ -0,0 +1,19 @@
+import { PiggyBank } from "lucide-react";
+import Link from "next/link";
+
+interface Props {
+  type: "normal" | "mobile";
+}
+
+function Logo({ type }: Props) {
+  return (
+    <Link href="/" className="flex items-center gap-2">
+      {type == "normal" && <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />}
+      <p className="bg-gradient-to-r from-amber-400 to-orange-500 bg-clip-text text-3xl font-bold leading-tight tracking-tighter text-transparent">
+        BudgetTracker
+      </p>
+    </Link>
+  );
+}
+
+export default Logo;
